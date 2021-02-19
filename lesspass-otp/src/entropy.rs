47167213d132e5adc955b4e06b7d26ac303f5fc5
Lolo_32 <lol.b@free.fr>
2021-02-19 04:27:56 +0100

    Entropy: little rewrite + expect

--- lesspass-otp/src/entropy.rs
+++ lesspass-otp/src/entropy.rs
@@ -1,23 +1,25 @@
 use num_bigint::BigUint;
 
-use crate::algo::Algorithm;
-use crate::hex::to_hex;
-use crate::master::Master;
+use crate::{algo::Algorithm, hex::to, master::Master};
 
+/// Entropy pool to derive the password
 #[derive(Debug, Clone)]
 pub struct Entropy(BigUint);
 
 impl Entropy {
     /// Return a salt, combining `site`, `login` and `counter` from strings.
+    #[must_use]
     pub fn salt(site: &str, login: &str, counter: u32) -> Vec<u8> {
         Self::salt_byte(site.as_bytes(), login.as_bytes(), &to_hex(counter))
     }
     /// Return a salt, combining `site`, `login` and `counter` from byte array.
+    #[must_use]
     pub fn salt_byte(site: &[u8], login: &[u8], counter: &[u8]) -> Vec<u8> {
         [site, login, counter].concat()
     }
 
     /// Generate the entropy, from the master password, a salt and a number of iterations
+    #[must_use]
     pub fn new(algorithm: Algorithm, master: &Master, salt: &[u8], iterations: u32) -> Self {
         Self(BigUint::from_bytes_be(&algorithm.pbkdf2(
             master.bytes(),
@@ -38,10 +40,7 @@ impl Entropy {
         let (quot, rem) = self.0.div_rem(len);
         self.0 = quot;
 
-        match rem.to_u64() {
-            Some(rem) => rem as usize,
-            None => unreachable!(),
-        }
+        rem.to_u64().expect("u64") as usize
     }
 }
 
@@ -51,8 +50,8 @@ mod tests {
 
     #[test]
     fn reference() {
-        let master = Master::new("tHis is a g00d! password", Algorithm::SHA256).unwrap();
-        let salt = Entropy::salt("lesspass.com", "♥", 1);
+        let master = Master::new("tHis is a g00d! password", Algorithm::SHA256).expect("lesspass");
+        let salt = Entropy::salt("lesspass.com", "\u{2665}", 1);
         let e = Entropy::new(Algorithm::SHA256, &master, &salt, 1);
         assert_eq!(
             e.0,
@@ -60,13 +59,13 @@ mod tests {
                 b"e99e20abab609cc4564ef137acb540de20d9b92dcc5cda58f78ba431444ef2da",
                 16,
             )
-            .unwrap()
+            .expect("biguint")
         );
     }
 
     #[test]
     fn another_reference_vector() {
-        let master = Master::new("password", Algorithm::SHA256).unwrap();
+        let master = Master::new("password", Algorithm::SHA256).expect("lesspass");
         let salt = Entropy::salt("example.org", "contact@example.org", 1);
         let e = Entropy::new(Algorithm::SHA256, &master, &salt, 100_000);
         assert_eq!(
@@ -75,7 +74,7 @@ mod tests {
                 b"dc33d431bce2b01182c613382483ccdb0e2f66482cbba5e9d07dab34acc7eb1e",
                 16,
             )
-            .unwrap()
+            .expect("biguint")
         );
     }
 }
